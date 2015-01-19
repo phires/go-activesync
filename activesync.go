30aafe0489b1da81c88144a74da3111cdf5c472d
Philipp <philipp@resch.email>
2015-01-19 14:58:05 +0100

    Device struct

---
+++ activesync.go
@@ -0,0 +1,16 @@
+// activesync project activesync.go
+package activesync
+
+type Device struct {
+	DeviceID                string
+	DeviceType              string
+	Model                   string
+	IMEI                    string
+	FriendlyName            string
+	OperatingSystem         string
+	OperatingSystemLanguage string
+	PhoneNumber             string
+	MobileOperator          string
+	UserAgent               string
+}
+
